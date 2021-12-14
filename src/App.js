b02c22d3593847e00bb975b9e4c649f47ad737d4
Igor Shadurin <igor.shadurin@gmail.com>
2021-12-14 14:48:14 +0300

    display file info

--- src/App.js
+++ src/App.js
@@ -1,51 +1,29 @@
 // import logo from './logo.svg';
 import './App.css';
-import Moment from 'moment'
 import FileBrowser, {Icons} from "react-keyed-file-browser";
 import {useEffect, useState} from "react";
 
 function App() {
     const [files, setFiles] = useState([]);
+    const [currentFile, setCurrentFile] = useState(null);
+
+    const getFileName = path => {
+        if (!path) {
+            return '';
+        }
+
+        const parts = path.split('/');
+
+        return parts[parts.length - 1]
+    }
 
     useEffect(() => {
-        setFiles([
-            {
-                key: 'photos/animals/cat in a hat.png',
-                modified: +Moment().subtract(1, 'hours'),
-                size: 1.5 * 1024 * 1024,
-            },
-            {
-                key: 'photos/animals/kitten_ball.png',
-                modified: +Moment().subtract(3, 'days'),
-                size: 545 * 1024,
-            },
-            {
-                key: 'photos/animals/elephants.png',
-                modified: +Moment().subtract(3, 'days'),
-                size: 52 * 1024,
-            },
-            {
-                key: 'photos/funny fall.gif',
-                modified: +Moment().subtract(2, 'months'),
-                size: 13.2 * 1024 * 1024,
-            },
-            {
-                key: 'photos/holiday.jpg',
-                modified: +Moment().subtract(25, 'days'),
-                size: 85 * 1024,
-            },
-            {
-                key: 'documents/letter chunks.doc',
-                modified: +Moment().subtract(15, 'days'),
-                size: 480 * 1024,
-            },
-            {
-                key: 'documents/export.pdf',
-                modified: +Moment().subtract(15, 'days'),
-                size: 4.2 * 1024 * 1024,
-                reference: 'test'
-            },
-        ])
+        async function run() {
+            let data = await (await fetch('files.json')).json();
+            setFiles(data)
+        }
+
+        run().then();
     }, []);
 
     return (
@@ -53,10 +31,12 @@ function App() {
             <h1 className="text-center">FairDataSociety Registry</h1>
             <div className="col-8">
                 <FileBrowser
+                    canFilter={false}
                     icons={Icons.FontAwesome(4)}
                     // onSelectFile={true}
                     onSelectFile={e => {
                         console.log(e);
+                        setCurrentFile(e);
                     }}
                     // headerRenderer={() => {
                     //     return <p>123123</p>;
@@ -72,8 +52,13 @@ function App() {
                     files={files}
                 />
             </div>
-            <div className="col-4">
-                Hello
+            <div className="col-4 App-file-info">
+                <p>File information</p>
+                {!currentFile && <p>...</p>}
+                {currentFile && <div>
+                    <p><strong>Name</strong>: {getFileName(currentFile.key)}</p>
+                    <p className="App-file-reference"><strong>Reference</strong>: {currentFile.reference}</p>
+                </div>}
             </div>
         </div>
     );
